f54b48d4070f82418e2401e2171fddab21443e80
nat254 <natalie.ndetei@strathmore.edu>
2024-03-31 01:06:12 +0200

    feat: add styles to an image

--- src/component/AboutPage.tsx
+++ src/component/AboutPage.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import picture from '../assets/images/kim-possible.png';
+import '../App.css';
 
 const AboutPage = () =>{
     return (
         <div>
             <h1>About Page</h1>
+            <img className="avatar" src={picture} alt="Kim Possible" />
             <p>Hey there. <br />How are you?</p>
         </div>
     );
