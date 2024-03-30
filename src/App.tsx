1a798c24e70cec950f2a575aa51285e47585c168
nat254 <natalie.ndetei@strathmore.edu>
2024-03-31 00:57:09 +0200

    feat: write markup with jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import MyButton from './component/MyButton';
+import AboutPage from './component/AboutPage';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Natalie's App</h1>
       <MyButton />
+      <AboutPage />
     </div>
   );
 }
---
+++ src/component/AboutPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () =>{
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>Hey there. <br />How are you?</p>
+        </div>
+    );
+};
+
+export default AboutPage;
