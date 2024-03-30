41ab6ec0b1b183977f5cb7a26467d3b123a992c3
nat254 <natalie.ndetei@strathmore.edu>
2024-03-31 01:21:47 +0200

    feat: try alternate way of conditional rendering

--- src/component/Conditional.tsx
+++ src/component/Conditional.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const Conditional = () => {
+    // let isGood = true;
+    // if (isGood) {
+    //     return <div>Good</div>;
+    // } else{
+    //     return <div>Bad</div>;
+    // };
     let isGood = true;
-    if (isGood) {
-        return <div>Good</div>;
-    } else{
-        return <div>Bad</div>;
-    };
+    return(
+       <div>
+        {isGood? (<p>Good</p>) : (<p>Bad</p>)}
+       </div>
+    );
 };
 
 export default Conditional;
