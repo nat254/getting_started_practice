f182534dadc71b768b5510aadba06ad298b329c2
nat254 <natalie.ndetei@strathmore.edu>
2024-03-31 19:50:03 +0300

    feat: add useState hook

--- src/component/MyButton.tsx
+++ src/component/MyButton.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
 
 const MyButton = () =>{
+
+    const[count, setCount]= useState(0);
+
     function handleClick(){
-        alert('Button clicked');
+        // alert('Button clicked');
+        setCount(count + 1);
     }
 
     return (
-        <button onClick = {handleClick}>I'm a button</button>
+        <button onClick = {handleClick}>You have clicked me {count} times</button>
     );
 };
 
