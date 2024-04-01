ac75fa3599b078c37071365921446a13662962a0
nat254 <natalie.ndetei@strathmore.edu>
2024-04-01 14:08:20 +0300

    feat: add html without enclosing in parent tags

---
+++ src/component/ToDoList.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ToDoList = () => {
+  return (
+    <h1>Hedy Lamarr's Todos</h1>
+     
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve spectrum technology</li>
+      </ul>
+  );
+};
+
+export default ToDoList;
